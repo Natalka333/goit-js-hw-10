c276a363df679cfa66c203a4d2c6a27c110f4a97
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-08-03 16:14:24 +0300

    Update index.js

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+// import axios from 'axios';
+
+// axios.defaults.headers.common['x-api-key'] =
+//   'live_ vaumoroPqd8p0uu6kFVMAXYOJOoT4v XhXIGYfCHRRSJYVpw49wZLNovtOafI japI';
+
+// const BASE_URL = `https://jsonplaceholder.typicode.com`;
+// let result = null;
+// fetch(`${BASE_URL}/posts?_limit=25&_page=2`)
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     result = data;
+//     console.log(data);
+//   })
+//   .catch(console.warn);
