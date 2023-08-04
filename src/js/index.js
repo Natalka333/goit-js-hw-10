e8a8143fd100caad97aa809e96a0920eb2bd03c1
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-08-04 11:47:44 +0300

    add css

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,15 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// axios.defaults.headers.common['x-api-key'] =
-//   'live_ vaumoroPqd8p0uu6kFVMAXYOJOoT4v XhXIGYfCHRRSJYVpw49wZLNovtOafI japI';
+const BASE_URL = `https://api.thecatapi.com/v1`;
+axios.defaults.headers.common['x-api-key'] =
+  'live_ vaumoroPqd8p0uu6kFVMAXYOJOoT4v XhXIGYfCHRRSJYVpw49wZLNovtOafI japI';
 
-// const BASE_URL = `https://jsonplaceholder.typicode.com`;
-// let result = null;
-// fetch(`${BASE_URL}/posts?_limit=25&_page=2`)
-//   .then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     return response.json();
-//   })
-//   .then(data => {
-//     result = data;
-//     console.log(data);
-//   })
-//   .catch(console.warn);
+export function fetchBreeds() {
+  return axios.get(`${BASE_URL}/breeds`).then(response => response.data);
+}
+
+export function fetchCatByBreed(breedId) {
+  return axios
+    .get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
+    .then(response => response.data);
+}
