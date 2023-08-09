1fb4498fa3f0bca89fdb1e540647119194ec4dff
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-08-09 16:30:33 +0300

    error

--- src/js/renderBreed.js
+++ src/js/renderBreed.js
@@ -1,11 +1,10 @@
 const renderBreed = ({ url, id, name, description, origin, temperament }) => {
-  return `<div class="cat-container"><img class="cat-picture" src="${url}" alt="${id}">
+  return `<div class="cat-container"><img class="cat-picture" src="${url}" alt="${id}"></div >
 		<div class="cat-box">
 	<h2 class="cat-info">${name}</h2>
   <p class="cat-desc"><b>Description:</b> ${description}</p>
   <p class="cat-temp"><b>Temperament:</b> ${temperament}</p>
-  <p class="cat-origin"><b>Origin:</b> ${origin}</p></div>
-	</div >`;
+  <p class="cat-origin"><b>Origin:</b> ${origin}</p></div>`;
 };
 
 export { renderBreed };
