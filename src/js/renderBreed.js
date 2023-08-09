077a805ef68af01f66d73a5c4323fb6d8c3d2070
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-08-09 13:41:14 +0300

    css stile

--- src/js/renderBreed.js
+++ src/js/renderBreed.js
@@ -1,10 +1,10 @@
 const renderBreed = ({ url, id, name, description, origin, temperament }) => {
   return `<div class="cat-container"><img class="cat-picture" src="${url}" alt="${id}">
 		<div class="cat-box">
-	<h2 class=cat-info>${name}</h2>
-  <p class="cat-desc">${description}</p>
-  <p class="cat-temp">${temperament}</p>
-  <p class="cat-origin">${origin}</p></div>
+	<h2 class="cat-info">${name}</h2>
+  <p class="cat-desc"><b>Description:</b> ${description}</p>
+  <p class="cat-temp"><b>Temperament:</b> ${temperament}</p>
+  <p class="cat-origin"><b>Origin:</b> ${origin}</p></div>
 	</div >`;
 };
 
