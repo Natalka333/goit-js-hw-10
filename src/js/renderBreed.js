e1dafbb1ae0553c012974511b5005e19c065c712
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-08-09 09:19:43 +0300

    Update renderBreed.js

--- src/js/renderBreed.js
+++ src/js/renderBreed.js
@@ -1,12 +1,12 @@
 import { catInfoEl } from './index';
 const renderBreed = breed => {
   const murkupPicture = `<img class="cat-picture" src="${breed.url}" alt="${breed.id}">`;
-  //   const murkupDescript = `<h2 class=cat-info>${breed.breeds[0].name}</h2>
-  // <p class="cat-desc">${breed.breeds[0].description}</p>
-  // <p class="cat-temp">${breed.breeds[0].temperament}</p>
-  // <p class="cat-origin">${breed.breeds[0].origin}</p>`;
+  const murkupDescript = `<h2 class=cat-info>${breed.breeds[0].name}</h2>
+  <p class="cat-desc">${breed[0].breeds[0].description}</p>
+  <p class="cat-temp">${breed[0].breeds[0].temperament}</p>
+  <p class="cat-origin">${breed[0].breeds[0].origin}</p>`;
   catInfoEl.insertAdjacentHTML('beforeend', murkupPicture);
-  // catInfoEl.insertAdjacentHTML('beforeend', murkupDescript);
+  catInfoEl.insertAdjacentHTML('beforeend', murkupDescript);
 };
 
 export { renderBreed };
