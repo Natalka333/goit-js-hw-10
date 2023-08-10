b05575666f1ae93f5c8e8b6b37a1ffcff8d49da8
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-08-10 15:42:37 +0300

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -8,27 +8,35 @@ const selectBreedEl = document.querySelector('.breed-select');
 const loadEl = document.querySelector('.loader');
 const catInfoEl = document.querySelector('.cat-info');
 
-fetchBreeds()
-  .then(breeds => {
-    const optionsEl = breeds
-      .map(({ id, name }) => {
-        return `<option value='${id}'>${name}</option>`;
-      })
-      .join('');
-    // console.log(breeds);
-    selectBreedEl.insertAdjacentHTML('beforeend', optionsEl);
-    new SlimSelect({
-      select: '.breed-select',
-    });
-  })
-  .catch(error => {
-    console.log(error);
-    Notiflix.Notify.failure(
-      'Oops! Something went wrong! Try reloading the page!'
-    );
-  });
+const renderFetchBreeds = () => {
+  loadEl.classList.add('unvisible');
 
+  fetchBreeds()
+    .then(breeds => {
+      const optionsEl = breeds
+        .map(({ id, name }) => {
+          return `<option value='${id}'>${name}</option>`;
+        })
+        .join('');
+      // console.log(breeds);
+      selectBreedEl.insertAdjacentHTML('beforeend', optionsEl);
+      new SlimSelect({
+        select: '.breed-select',
+      });
+    })
+    .catch(error => {
+      console.log(error);
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      );
+    })
+    .finally(() => {
+      loadEl.classList.add('unvisible');
+    });
+};
 selectBreedEl.addEventListener('change', handleChangeSelect);
+
+renderFetchBreeds();
 
 function handleChangeSelect(event) {
   const breedId = event.target.value;
@@ -48,5 +56,8 @@ function handleChangeSelect(event) {
       console.log(error);
       catInfoEl.innerHTML = '';
       Notiflix.Notify.failure('There is not information about this cat');
+    })
+    .finally(() => {
+      loadEl.classList.add('unvisible');
     });
 }
